60de697ac6cf445a898079830c0a3913e68611a7
Tony Malagisi <mtb3000gt@gmail.com>
2015-01-13 16:04:43 -0500

    Plain-ify

--- plain.mk
+++ plain.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/d2bst/full_d2bst.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/plain/config/nfc_enhanced.mk)
 
-# Inherit some common LiquidSmooth stuff.
-$(call inherit-product, vendor/liquid/config/common_phone.mk)
+# Inherit some common PlainAndy stuff.
+$(call inherit-product, vendor/plain/config/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2bst \
@@ -12,6 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2bst/d2bst:5.0.2/LRX22G/L710TVPU0ANF4:user/release-keys" \
     PRIVATE_BUILD_DESC="d2bst-user 5.0.2 LRX22G L710TVPU0ANF4 release-keys"
 
-PRODUCT_NAME := liquid_d2bst
+PRODUCT_NAME := plain_d2bst
 PRODUCT_DEVICE := d2bst
 
